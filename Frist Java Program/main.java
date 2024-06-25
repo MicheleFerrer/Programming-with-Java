6973915fb2e7c0343a684b64c7ddf02865aa3bb4
MicheleFerrer <michele.e.ferrer@gmail.com>
2024-06-25 07:12:44 -0400

    using type casting to create program comparing a users score to max score in a game. I manually converted from int to float point

--- Frist Java Program/main.java
+++ Frist Java Program/main.java
@@ -6,6 +6,14 @@ public class main{
 String name = "daniela";
 System.out.print("daniela is ");
 int age = 30;
-System.out.print(age);
-} 
+System.out.println(age);
+    
+
+
+        int maxScore = 500;
+        int userScore = 407;
+
+        float percentage = (float) userScore / maxScore * 100.0f;
+        System.out.println("user's percentage is " + percentage);
+    }
 }
