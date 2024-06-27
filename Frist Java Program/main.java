aebf6bbe892053604421012fc610b43978a26c5d
MicheleFerrer <michele.e.ferrer@gmail.com>
2024-06-27 06:10:59 -0400

    Using object oriented programming in declaring instance variables, creating instance of the main class, assigning values to the instance variables, to have easy to use/read code.

--- Frist Java Program/main.java
+++ Frist Java Program/main.java
@@ -1,18 +1,30 @@
 
 public class main{
+    //Declaring instance variables
+    String name;
+    String identity;
+    int age;
+    
    public static void main(String[] args) {
     System.out.println("hi world");
     
-String name = "daniela";
-int age = 30;
-System.out.println("daniela is " + age);
-int i = 0;
+//String name = "daniela";
+//int age = 30;
+main myObj = new main();
+myObj.name = "daniela ";
+myObj.identity = "is : ";
+myObj.age = 25;
+System.out.print(myObj.name);
+System.out.print(myObj.identity);
+System.out.println(myObj.age);
+
+//System.out.println("daniela is " + age);
+/*int i = 0;
 while (i < 5) {
     System.out.println(i);
     i++;
-}
+}*/
  
-
 
         int maxScore = 500;
         int userScore = 407;
