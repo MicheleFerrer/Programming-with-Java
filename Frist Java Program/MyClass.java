961b041e2f310dfbf2a795ce20e82d3cb385cb05
MicheleFerrer <michele.e.ferrer@gmail.com>
2024-06-27 09:55:32 -0400

    Using GUI and java packages to import a class and created a intergrated input for username and response

---
+++ Frist Java Program/MyClass.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+class MyClass{
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter Username");
+
+        String userName = myObj.nextLine();
+        System.out.println("Username is: " + userName);
+    }
+}
