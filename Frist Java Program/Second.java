4b794e822fa380ebe1982c020bff73b6fd99fa8b
MicheleFerrer <michele.e.ferrer@gmail.com>
2024-07-02 08:45:50 -0400

    Creating a program that adds two numbers with user input by importing the Scanner class

--- Frist Java Program/Second.java
+++ Frist Java Program/Second.java
@@ -11,13 +11,23 @@ public class Second{
         System.out.println("After time formatting: " + formattedTime);
     }
 }*/
-
+import java.util.Scanner; 
 public class Second {
     public static void main(String[] args) {
-         int x = 5;
+        int x, y, sum;
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Type a number:");
+        x = myObj.nextInt();
+
+        System.out.println("Type another number:");
+        y = myObj.nextInt();
+
+        sum = x + y;
+        System.out.print("The sum of two numbers is: " + sum);
+    /*int x = 5;
     int y = 10;
     int sum = x + y;
-    System.out.println(sum);
+    System.out.println(sum);*/
     }
 }
 
