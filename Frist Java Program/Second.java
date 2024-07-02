b17dd2ca85ce27d5b75939736cc86243782ceb4d
MicheleFerrer <michele.e.ferrer@gmail.com>
2024-07-02 08:12:31 -0400

    sum of two numbers

--- Frist Java Program/Second.java
+++ Frist Java Program/Second.java
@@ -1,4 +1,4 @@
-import java.time.LocalDateTime;
+/*import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Second{
@@ -10,4 +10,15 @@ public class Second{
         String formattedTime = myObj.format(myFormatObj);
         System.out.println("After time formatting: " + formattedTime);
     }
+}*/
+
+public class Second {
+    public static void main(String[] args) {
+         int x = 5;
+    int y = 10;
+    int sum = x + y;
+    System.out.println(sum);
+    }
 }
+
+
