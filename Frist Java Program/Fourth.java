a8554af57fec5c0c3f373721231e0917cd011d83
MicheleFerrer <michele.e.ferrer@gmail.com>
2024-07-02 06:31:14 -0400

    using lambda expressions and consumer interface to store lambda expressions in a variable

--- Frist Java Program/Fourth.java
+++ Frist Java Program/Fourth.java
@@ -9,7 +9,7 @@ static void checkAge(int age) {
 public static void main(String[] args) {
     checkAge(15);
 }
-}*/
+}
 
 public class Fourth extends Thread {
     public static void main(String[] args) {
@@ -21,4 +21,19 @@ public class Fourth extends Thread {
       System.out.println("this code is running in a thread");
      }
   }
+*/
 
+import java.util.ArrayList;
+import java.util.function.Consumer;
+
+public class Fourth {
+  public static void main(String[] args) {
+    ArrayList<Integer> numbers = new ArrayList<Integer>();
+    numbers.add(5);
+    numbers.add(9);
+    numbers.add(8);
+    numbers.add(1);
+    Consumer<Integer> method = (n) -> { System.out.println(n); };
+    numbers.forEach( method );
+  }
+}
