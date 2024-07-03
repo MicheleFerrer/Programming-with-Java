5f2a4ac75110a6fb761493e78e70e0db79f4b967
MicheleFerrer <michele.e.ferrer@gmail.com>
2024-07-03 05:46:27 -0400

    Calculating the sum of arrays

--- Frist Java Program/Sixth.java
+++ Frist Java Program/Sixth.java
@@ -1,2 +1,15 @@
 // Practice Program: Sum of an Array
+public class Sixth{
+    public static void main(String[] args) {
+    int [] myArray = {2, 4, 6, 8};
+    int i;
+    int sum = 0;
+// Loop through an array elements to calculate the sum:
+for (i = 0; i < myArray.length; i++) {
+    sum += myArray[i];
+}
+// Print the sum of array elements:
+    System.out.println("The sum of the array is:" + sum); 
+    }
 
+}
