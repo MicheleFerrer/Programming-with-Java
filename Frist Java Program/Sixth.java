1f49c7eeb6a6c71acf74aed7875872ce23167a46
MicheleFerrer <michele.e.ferrer@gmail.com>
2024-07-04 11:59:03 -0400

    working on adding sum of an array at specific intervals in the array

--- Frist Java Program/Sixth.java
+++ Frist Java Program/Sixth.java
@@ -7,9 +7,16 @@ public class Sixth{
 // Loop through an array elements to calculate the sum:
 for (i = 0; i < myArray.length; i++) {
     sum += myArray[i];
+} if (sum > 7) {
+break;
+System.out.println("The sum of the array is 7");    
 }
+else if {
+
+
 // Print the sum of array elements:
     System.out.println("The sum of the array is:" + sum); 
     }
+}
 
 }
